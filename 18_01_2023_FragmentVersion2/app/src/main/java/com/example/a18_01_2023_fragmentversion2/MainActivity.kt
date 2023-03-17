29aeb931084d294787edde0c2327517fe403e507
aishwarya <ashp17star@gmail.com>
2023-03-17 14:18:33 +0530

    Updated

--- 18_01_2023_FragmentVersion2/app/src/main/java/com/example/a18_01_2023_fragmentversion2/MainActivity.kt
+++ 18_01_2023_FragmentVersion2/app/src/main/java/com/example/a18_01_2023_fragmentversion2/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.a18_01_2023_fragmentversion2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.a18_01_2023_fragmentversion2.databinding.ActivityMainBinding
+import com.example.a18_01_2023_fragmentversion2.fragments.CounterFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var counterFragment: CounterFragment
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initializeFragments()
+        initListeners()
+    }
+    private fun initializeFragments(){
+        counterFragment = supportFragmentManager.findFragmentById(R.id.counterFragment) as CounterFragment
+    }
+    private fun initListeners(){
+        binding.btnAddCounterFragment.setOnClickListener {
+
+        }
     }
 }
--- 18_01_2023_FragmentVersion2/app/src/main/java/com/example/a18_01_2023_fragmentversion2/fragments/CounterFragment.kt
+++ 18_01_2023_FragmentVersion2/app/src/main/java/com/example/a18_01_2023_fragmentversion2/fragments/CounterFragment.kt
@@ -1,5 +1,6 @@
 package com.example.a18_01_2023_fragmentversion2.fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,11 +11,41 @@ import com.example.a18_01_2023_fragmentversion2.databinding.CounterFragmentBindi
 class CounterFragment : Fragment() {
     private lateinit var binding : CounterFragmentBinding
 
+    var count = 0
+    set(value){
+        field = value
+        binding.txtCount.text = "${count}"
+    }
+
+    var title : String = ""
+    set(value){
+        field = value
+        binding.txtTitle.text = title
+    }
+
+    @SuppressLint("UseRequireInsteadOfGet")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        binding = CounterFragmentBinding.inflate(inflater)
+        if(arguments != null){
+            binding.txtTitle.text = arguments!!.getString("title")
+            binding.txtCount.text = "${arguments!!.getInt("count")}"
+        }
+
+        initListeners()
+        return binding.root
+    }
+
+    private fun initListeners(){
+        binding.btnDecrement.setOnClickListener {
+            binding.txtCount.text = "${--count}"
+        }
+
+        binding.btnIncrement.setOnClickListener {
+            binding.txtCount.text = "${++count}"
+        }
     }
 }
