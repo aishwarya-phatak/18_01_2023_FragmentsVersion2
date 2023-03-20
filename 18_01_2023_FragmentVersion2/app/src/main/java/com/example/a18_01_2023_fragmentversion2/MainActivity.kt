b0d4729935412fa7337c927a3c74de82f95e2f77
aishwarya <ashp17star@gmail.com>
2023-03-20 14:09:12 +0530

    FragmentTransaction

--- 18_01_2023_FragmentVersion2/app/src/main/java/com/example/a18_01_2023_fragmentversion2/MainActivity.kt
+++ 18_01_2023_FragmentVersion2/app/src/main/java/com/example/a18_01_2023_fragmentversion2/MainActivity.kt
@@ -8,6 +8,7 @@ import com.example.a18_01_2023_fragmentversion2.fragments.CounterFragment
 class MainActivity : AppCompatActivity() {
     private lateinit var counterFragment: CounterFragment
     private lateinit var binding : ActivityMainBinding
+    private var counterFragmentList = ArrayList<CounterFragment>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,8 +21,41 @@ class MainActivity : AppCompatActivity() {
         counterFragment = supportFragmentManager.findFragmentById(R.id.counterFragment) as CounterFragment
     }
     private fun initListeners(){
-        binding.btnAddCounterFragment.setOnClickListener {
 
+        binding.btnReset.setOnClickListener {
+                    counterFragment.count = binding.edtCount.text.toString().toInt()
+            for(counterFragment in counterFragmentList){
+                counterFragment.count = binding.edtCount.text.toString().toInt()
+            }
         }
+
+        binding.btnAddCounterFragment.setOnClickListener {
+                var counterFragment = CounterFragment()
+                    var input = Bundle()
+            input.putString("title",binding.edtCounterFragmentTitle.text.toString())
+            input.putInt("count",binding.edtCount.text.toString().toInt())
+            counterFragment.arguments = input;
+
+            //FragmentTransaction
+            var fragmentTransition = supportFragmentManager.beginTransaction()
+            fragmentTransition.add(R.id.mainContainer,counterFragment,null)
+            fragmentTransition.commit()
+            counterFragmentList.add(counterFragment)
+        }
+
+        binding.btnRemoveCounterFragment.setOnClickListener {
+            if(counterFragmentList.size == 0){
+                return@setOnClickListener
+            }
+            supportFragmentManager.beginTransaction()
+                .remove(counterFragmentList[0])
+                .commit()
+            counterFragmentList.removeAt(0)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        counterFragment.count = 0
     }
 }
